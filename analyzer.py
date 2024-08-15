a4b22ca4910f336cfd71ec6f317bd03b9c71494d
DARJA JEPIFANOVA <ydarja@gmail.com>
2024-08-15 13:50:24 +0200

    compare with morethansentiments

--- analyzer.py
+++ analyzer.py
@@ -4,6 +4,7 @@ import torch
 from torch.utils.data import DataLoader, Dataset
 from transformers import BertTokenizer
 from scipy.stats import pearsonr, spearmanr
+from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
@@ -13,9 +14,10 @@ import matplotlib.pyplot as plt
 import MoreThanSentiments as mts
 
 class TestDataset(Dataset):
-    def __init__(self, df, tokenizer):
+    def __init__(self, df, tokenizer, max_length=128):
         self.df = df
         self.tokenizer = tokenizer
+        self.max_length = max_length
 
     def __len__(self):
         return len(self.df)
@@ -26,10 +28,10 @@ class TestDataset(Dataset):
         depth1 = self.df.iloc[idx]['depth1']
         depth2 = self.df.iloc[idx]['depth2']
 
-        encoding1 = self.tokenizer(sentence1, return_tensors='pt', padding='max_length', truncation=True, max_length=128)
-        encoding2 = self.tokenizer(sentence2, return_tensors='pt', padding='max_length', truncation=True, max_length=128)
+        encoding1 = self.tokenizer(sentence1, return_tensors='pt', padding='max_length', truncation=True, max_length=self.max_length)
+        encoding2 = self.tokenizer(sentence2, return_tensors='pt', padding='max_length', truncation=True, max_length=self.max_length)
         
-        item = {
+        return {
             'input_ids1': encoding1['input_ids'].squeeze(0),
             'attention_mask1': encoding1['attention_mask'].squeeze(0),
             'input_ids2': encoding2['input_ids'].squeeze(0),
@@ -39,56 +41,56 @@ class TestDataset(Dataset):
             'sentence1': sentence1,
             'sentence2': sentence2
         }
-        return item
 
-def get_specificity_scores(test_df):
+def get_specificity_scores(test_df, model_path='models/model3', batch_size=4, max_length=128):
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    test_dataset = TestDataset(test_df, tokenizer)
-    test_loader = DataLoader(test_dataset, batch_size=4, shuffle=False)
+    test_dataset = TestDataset(test_df, tokenizer, max_length)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
     # Load the model
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = SpecificityModel().to(device)
     optimizer = torch.optim.Adam(model.parameters())
-    model, optimizer, epoch = load_model(model, optimizer, 'models/model1', device)
+    model, optimizer, epoch = load_model(model, optimizer, model_path, device)
     model.eval()
 
-    specificity_scores = []
-    true_depths = []
-    test_sentences = []
+    specificity_scores, true_depths, test_sentences = [], [], []
 
     with torch.no_grad():
         for batch in test_loader:
+            input_ids1 = batch['input_ids1'].to(device)
+            attention_mask1 = batch['attention_mask1'].to(device)
+            input_ids2 = batch['input_ids2'].to(device)
+            attention_mask2 = batch['attention_mask2'].to(device)
+            depth1 = batch['depth1'].to(device)
+            depth2 = batch['depth2'].to(device)
+                
+            logits1, logits2 = model(input_ids1, attention_mask1, input_ids2, attention_mask2)
+                
+            scores1 = logits1.squeeze().cpu().numpy()
+            scores2 = logits2.squeeze().cpu().numpy()
+                
             for idx in range(len(batch['sentence1'])):
                 sentence1 = batch['sentence1'][idx]
                 sentence2 = batch['sentence2'][idx]
-                test_sentences.append(sentence1)
-                test_sentences.append(sentence2)
-        
-                input_ids1 = batch['input_ids1'].to(device)
-                attention_mask1 = batch['attention_mask1'].to(device)
-                input_ids2 = batch['input_ids2'].to(device)
-                attention_mask2 = batch['attention_mask2'].to(device)
-                depth1 = batch['depth1'].to(device)
-                depth2 = batch['depth2'].to(device)
                 
-                logits1, logits2 = model(input_ids1, attention_mask1, input_ids2, attention_mask2)
-                
-                scores1 = logits1.squeeze().cpu().numpy()
-                scores2 = logits2.squeeze().cpu().numpy()
-                specificity_scores.extend(scores1)
-                specificity_scores.extend(scores2)
-                true_depths.extend(depth1.cpu().numpy())
-                true_depths.extend(depth2.cpu().numpy())
+                test_sentences.extend([sentence1, sentence2])
+                specificity_scores.extend([scores1[idx], scores2[idx]])
+                true_depths.extend([depth1[idx].cpu().numpy(), depth2[idx].cpu().numpy()])
 
     specificity_scores = np.array(specificity_scores)
+    # normalize (0,1)
+    scaler = MinMaxScaler()
+    specificity_scores =  scaler.fit_transform(specificity_scores.reshape(-1, 1)).flatten()
     true_depths = np.array(true_depths)
-    
 
     return specificity_scores, true_depths, test_sentences
 
+
 def analyze(test_df):
-    specificity_scores, true_depths, test_sentences = get_specificity_scores(test_df) 
+    specificity_scores, true_depths, test_sentences = get_specificity_scores(test_df)
+
+    '''
     # Calculate correlations
     pearson_corr, _ = pearsonr(specificity_scores, true_depths)
     spearman_corr, _ = spearmanr(specificity_scores, true_depths)
@@ -110,26 +112,31 @@ def analyze(test_df):
     print("Regression Analysis")
     print(regression_model.summary())
 
-    # Scatter plot
+    # Scatter Plot
     plt.figure(figsize=(8, 6))
     plt.scatter(specificity_scores, true_depths)
     plt.xlabel('Specificity Scores', fontsize=12)
     plt.ylabel('True Depths', fontsize=12)
     plt.title('Scatter Plot of Specificity Scores vs True Depths', fontsize=14)
     plt.grid(True)
+    # SAVE
     plt.savefig('plots/scatter_plot.png', dpi=300)
+    plt.close()
 
-    # Regression plot
+    # Regression Plot
     plt.figure(figsize=(8, 6))
-    plt.plot(specificity_scores, regression_model.predict(X), color='red')
-    plt.scatter(specificity_scores, true_depths)
+    plt.scatter(specificity_scores, true_depths, label='Data points')
+    plt.plot(specificity_scores, regression_model.predict(X), color='red', label='Regression Line')
     plt.xlabel('Specificity Scores', fontsize=12)
     plt.ylabel('True Depths', fontsize=12)
     plt.title('Regression Analysis: Specificity Scores vs True Depths', fontsize=14)
     plt.grid(True)
+    plt.legend()
+    # SAVE
     plt.savefig('plots/regression_plot.png', dpi=300)
-
-    # Boxplots by depth plot
+    plt.close()
+    '''
+    # Boxplots by Depth
 
     specific_depths = [2,6,10,14]
 
@@ -139,19 +146,41 @@ def analyze(test_df):
         filtered_depths = true_depths[mask]
         plt.figure(figsize=(10, 6))
         plt.boxplot(filtered_scores, vert=False)
+        plt.xlim(-10, 11)
         plt.xlabel('Specificity Scores', fontsize=12)
         plt.ylabel('True Depths', fontsize=12)
         plt.title(f'Specificity Scores at WordNet Depth {depth}', fontsize=14)
         plt.grid(True)
         plt.savefig(f'plots/boxplot_depth{depth}.png', dpi=300)
         plt.show()
+        plt.close()
+    '''
+    # Comparison with Other Library
+    other_specificity_scores = mts.Specificity(test_sentences)
+    correlation_with_other, _ = pearsonr(specificity_scores, other_specificity_scores)
+    print(f'Correlation with other library\'s specificity scores: {correlation_with_other}')
 
+    # Comparison Plot
+    plt.figure(figsize=(10, 6))
+    plt.scatter(specificity_scores, other_specificity_scores, color='blue', label='Data points', s=50, alpha=0.6)
 
-    # Comparison with OtherThanSentiments
-    #other_specificity_scores = mts.Specificity(test_sentences)
-    #correlation_with_other = pearsonr(specificity_scores, other_specificity_scores)
-    #print(f'Correlation with other library\'s specificity scores: {correlation_with_other}')
+    # Fit a line using NumPy
+    coefficients = np.polyfit(specificity_scores, other_specificity_scores, 1)
+    polynomial = np.poly1d(coefficients)
+    regression_line = polynomial(specificity_scores)
 
+    # Plot the regression line
+    plt.plot(specificity_scores, regression_line, color='red', linewidth=2, label='Regression Line')
+
+    plt.xlabel('Your Specificity Scores')
+    plt.ylabel('Other Library\'s Specificity Scores')
+    plt.title('Comparison of Specificity Scores')
+    plt.legend()
+    plt.grid(True)
+    
+    plt.savefig('plots/otherthansentiments.png', dpi=300)
+    plt.close()
+    '''
 if __name__ == "__main__":
-    _, _, test_df = load_data_from_csv('data/balanced_raw_synsets.csv')
-    analyze(test_df)
+    _, _, test = load_data_from_csv('data/balanced_raw_synsets.csv')
+    analyze(test)
